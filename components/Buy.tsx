deb77fa96a3bf5f31b27999d3d17bfcaeb92be72
Trishane <trishaneww@gmail.com>
2025-01-29 10:13:51 -0500

    updated partnership margin

--- components/Buy.tsx
+++ components/Buy.tsx
@@ -6,16 +6,16 @@ const Buy = () => {
     <div className="flex flex-col items-center gap-12 mt-32">
         <p className="text-3xl md:text-5xl lg:text-7xl font-bold secondary">HOW TO BUY</p>
         <section className="grid grid-cols-1 md:grid-cols-3 gap-6 px-6 md:w-[80%]">
-            <div className="flex flex-col items-center justify-center text-left text-lg gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-8 px-6">
-                <p className="text-2xl lg:text-4xl">CREATE A WALLET</p>
+            <div className="flex flex-col items-center justify-start text-left text-lg gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-8 px-6">
+                <p className="text-2xl lg:text-4xl w-full">CREATE A WALLET</p>
                 <p>Similar to the charm of Shiba Inu on Ethereum, $HAMI brings a delightful twist with its adorable hamster-themed allure. Dive into the whimsical universe of $HAMI, where crypto merges with humor, and embark on an exciting journey toward the next big meme coin sensation on SOLANA</p>
             </div>
-            <div className="flex flex-col items-center justify-center text-left text-lg gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-8 px-6">
-                <p className="text-2xl lg:text-4xl">CREATE A WALLET</p>
+            <div className="flex flex-col items-center justify-start text-left text-lg gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-8 px-6">
+                <p className="text-2xl lg:text-4xl w-full">CREATE A WALLET</p>
                 <p>Similar to the charm of Shiba Inu on Ethereum, $HAMI brings a delightful twist with its adorable hamster-themed allure. Dive into the whimsical universe of $HAMI, where crypto merges with humor, and embark on an exciting journey toward the next big meme coin sensation on SOLANA</p>
             </div>
-            <div className="flex flex-col items-center justify-center text-left text-lg gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-8 px-6">
-                <p className="text-2xl lg:text-4xl">CREATE A WALLET</p>
+            <div className="flex flex-col items-center justify-start text-left text-lg gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-8 px-6">
+                <p className="text-2xl lg:text-4xl w-full">CREATE A WALLET</p>
                 <p>Similar to the charm of Shiba Inu on Ethereum, $HAMI brings a delightful twist with its adorable hamster-themed allure. Dive into the whimsical universe of $HAMI, where crypto merges with humor, and embark on an exciting journey toward the next big meme coin sensation on SOLANA</p>
             </div>
         </section>
--- components/Partnership.tsx
+++ components/Partnership.tsx
@@ -6,7 +6,7 @@ const Partnership = () => {
   return (
     <div className="flex flex-col items-center gap-2 mt-32 px-6">
         <p className="text-3xl md:text-5xl lg:text-7xl font-bold secondary">PARTNERSHIP</p>
-        <p className="text-center">$PLAT IS AVAILABLE TO BE TRADED ON MAJOR PLATFORMS</p>
+        <p className="text-center text-base lg:text-xl">$PLAT Is Available To Be Traded On Major Platforms</p>
         <section className="grid grid-cols-2 md:grid-cols-3 gap-2 lg:gap-6 md:w-[55%] md:min-w-[700px] lg:min-w-[900px] mt-8">
             <div className="flex flex-col items-center justify-center gap-6 bg-slate-50 border-[1.5px] border-black rounded-lg w-full max-w-[450px] py-4 px-6">
                 <Image
